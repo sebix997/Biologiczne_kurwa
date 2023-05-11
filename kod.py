f9fb97551831f438c5d379e2e8c6c68813fef5fb
Sebastian Ptak <128471562+sebix997@users.noreply.github.com>
2023-05-11 13:01:25 +0200

    Add files via upload
    Ten jest skończony nie tykaj kurwa

--- kod.py
+++ kod.py
@@ -9,55 +9,9 @@ x=[]
 y=[]
 g=[]
 
-
-def float_bin(number, places):
-    # split() separates whole number and decimal
-    # part and stores it in two separate variables
-    whole, dec = str(number).split(".")
-
-    # Convert both whole number and decimal
-    # part from string type to integer type
-    whole = int(whole)
-    dec = int(dec)
-
-    # Convert the whole number part to it's
-    # respective binary form and remove the
-    # "0b" from it.
-    res = bin(whole).lstrip("0b") + "."
-
-    # Iterate the number of times, we want
-    # the number of decimal places to be
-    for x in range(places):
-        # Multiply the decimal value by 2
-        # and separate the whole number part
-        # and decimal part
-        whole, dec = str((decimal_converter(dec)) * 2).split(".")
-
-        # Convert the decimal part
-        # to integer again
-        dec = int(dec)
-
-        # Keep adding the integer parts
-        # receive to the result variable
-        res += whole
-
-    return res
-
-
-# Function converts the value passed as
-# parameter to it's decimal representation
-def decimal_converter(num):
-    while num > 1:
-        num /= 10
-    return num
-
-
-
-
-
 for i in range (0,int(n)):
-    x.append(random.randrange(1, 31)+random.random()) ##tworzenie tablicy z wartosciami od 1 do 31
-    g.append(float_bin(x[i],2)) ## zamian na binarne
+    x.append(random.randrange(1, 31)) ##tworzenie tablicy z wartosciami od 1 do 31
+    g.append(str(format(x[i],'b').zfill(5))) ## zamian na binarne
     y.append((x[i]/15)*(math.sin((math.pi*x[i])/8)+1)) ##wynik funkcji dla danej wartosci
 ###sprawdzenie tablic
 print(x)
@@ -100,11 +54,9 @@ for i in range (0,pow):
                                                             ## tak dlugo az wylosowane zmienna jak_dlugo
 
     dziecko1=str(dziecko1)
+
     g.append(dziecko1)
-    dziecko1=int(dziecko1,2)
-
-
-    e=float_bin(dziecko1,2)
+    e=int(dziecko1,2)
     y.append((e / 15) * (math.sin((math.pi * e) / 8)))
     x.append(e)
 
